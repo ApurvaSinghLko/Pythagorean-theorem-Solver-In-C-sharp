7ecd1b8965744010bd3ab84715effffb570a9b41
Apurva Singh <88497948+ApurvaSinghLko@users.noreply.github.com>
2021-11-05 13:47:43 +0530

    Pythagorean theorem Solver In C sharp
    This is the Pythagorean theorem Solver In C sharp. this project is made in VS 2019. it calculates the Hypotenuse of a right angle triangle.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Pythagorean_theorem_Solver_In_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pythagorean theorem in C# 
+            Console.Title = "Pythagorean theorem By Apurva Singh";
+            Console.WriteLine("This Project's Source Code Is Also Available On My Website https://apurvasingh.ml");
+            Console.WriteLine("Pythagorean theorem referrs to Side A Squared + Side B Squared = Hypotenuse Squared ");
+            Console.WriteLine("Note: Please Enter Single Units Only. Decimal Numbers and words Are Currently Isn't Allowed. Thanks ");
+            Console.WriteLine("Please Enter Side A");
+            string SideAasString = Console.ReadLine();
+            int SideA = Int32.Parse(SideAasString);
+            Console.WriteLine("Sucessfully Entered the value of Side A. You Have Entered " + SideA);
+            Console.WriteLine("Please Enter Side B");
+            string SideBasString = Console.ReadLine();
+            int SideB = Int32.Parse(SideBasString);
+            Console.WriteLine("Sucessfully Entered the value of Side B. You Have Entered " + SideB);
+            int SideASQ = SideA * SideA;
+            int SideBSQ = SideB * SideB;
+            int Final = SideASQ + SideBSQ;
+
+            Console.WriteLine("Hypotenuse Is");
+            Console.Write(Math.Sqrt(Final));
+            Console.WriteLine(" Units");
+            Console.WriteLine("Thanks For Using");
+            Console.ReadLine();
+        }
+    }
+}
